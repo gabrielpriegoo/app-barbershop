15a5c3c4cb7f41611b325adf878321f5ca8add6e
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-08-09 08:45:28 -0300

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Click</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Usuário!</h2>
+        <p>Seja bem-vindo(a)!</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative h-[150px] w-full">
+          <Image
+            alt="Agende seu horario"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
