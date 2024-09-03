237727f83f8ce816c429eed5016e0a1e72b92763
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-09-03 10:25:09 -0300

    feat: :sparkles: ensure only signed in user can book and sign out

---
+++ src/app/_components/sign-in-dialog.tsx
@@ -0,0 +1,25 @@
+import { signIn } from "next-auth/react"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+
+const SignInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça login na plataforma</DialogTitle>
+        <DialogDescription>
+          Conecte-se usando sua conta Google
+        </DialogDescription>
+      </DialogHeader>
+
+      <Button className="gap-1 font-bold" onClick={handleLoginWithGoogleClick}>
+        <Image alt="Google" src="/google.svg" width={18} height={18} />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SignInDialog
