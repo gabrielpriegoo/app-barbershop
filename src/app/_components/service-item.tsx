c91b466456a9bd33533a5a46d3b4d38051b3b3e0
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-09-03 19:05:56 -0300

    feat: :sparkles: ensure only authenticated user can book

--- src/app/_components/service-item.tsx
+++ src/app/_components/service-item.tsx
@@ -125,7 +125,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
       toast.success("Reserva criada com sucesso!")
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
       handleBookingSheetOpenChange()
